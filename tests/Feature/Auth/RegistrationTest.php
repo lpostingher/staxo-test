bbd22a49e84383fe18ecf5c17cf80b9dce4945e8
Lucas Postingher <lucaspostingher@outlook.com>
2023-03-22 21:42:49 -0300

    PHP Unit

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Auth;
 
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -12,14 +13,16 @@ class RegistrationTest extends TestCase
 
     public function test_registration_screen_can_be_rendered(): void
     {
-        $response = $this->get('/register');
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get('/register');
 
         $response->assertStatus(200);
     }
 
     public function test_new_users_can_register(): void
     {
-        $response = $this->post('/register', [
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->post('/register', [
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => 'password',
@@ -28,5 +31,24 @@ class RegistrationTest extends TestCase
 
         $this->assertAuthenticated();
         $response->assertRedirect(RouteServiceProvider::HOME);
+    }
+
+    public function test_registration_screen_cant_be_rendered(): void
+    {
+        $response = $this->get('/register');
+
+        $response->assertRedirect(route('login'));
+    }
+
+    public function test_new_users_cant_register(): void
+    {
+        $response = $this->post('/register', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response->assertRedirect(route('login'));
     }
 }
